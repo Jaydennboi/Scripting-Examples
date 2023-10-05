f9ec4399429c2efe33f15f5642310f28422b9e71
Jaydennboi <Jaydenmarques098@riversideunified.org>
2023-10-05 11:53:40 -0700

    updated variable script and created functions script

--- Scripting examples/Assets/Scripts/Variable.cs
+++ Scripting examples/Assets/Scripts/Variable.cs
@@ -2,25 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Variable : MonoBehaviour
+public class Variables : MonoBehaviour
 {
+    // Declaring Variables
     // Public Member/Gobal Variables
     public float MoveSpeed = 10f;
     public int PlayerScore = 0;
     public string PlayerName = "John Doe";
     public bool IsPlayerActive = true;
 
-    // Private Meme
-    private
+    // Private Member/Gobal Variables
+    private float _maxSpeed = 100f;
+    private int _totalScore = 0;
+    private string _userName = "John007";
+    private bool _isGameOver = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
 
+    }
     // Update is called once per frame
     void Update()
     {
-        
+
+        // Declaring local variables
+        float horizontalInput = Input.GetAxis("Horizontal");
+    }
+
+    private void UpdateScore(int amount)
+    {
+        // using a local variable to update the total score
+        _totalScore += amount;
     }
 }
